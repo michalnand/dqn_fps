dcdcba25bac60de2e8ed11525f7d2c7d70d564b7
michalnand <michal.nand@gmail.com>
2019-05-30 20:08:44 +0200

    common

--- src/level_test.cpp
+++ src/level_test.cpp
@@ -10,6 +10,10 @@ int main()
     while (1)
     {
         visualisation.start();
+
+        visualisation.set_color(1.0, 1.0, 1.0);
+        visualisation.paint_textured_rectangle(1, 1, 63);
+
         visualisation.finish();
     }
 
